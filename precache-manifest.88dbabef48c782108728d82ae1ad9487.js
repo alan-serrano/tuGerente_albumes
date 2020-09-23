7b79d8ee76a28b6136356db5801aa0c939f0f635
alan-serrano <alan.serrano.pch@gmail.com>
2020-09-23 19:02:09 -0400

    Updates

---
+++ precache-manifest.88dbabef48c782108728d82ae1ad9487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7723c465ef4af95a73034b4547c4fe7f",
+    "url": "/tuGerente_albumes/index.html"
+  },
+  {
+    "revision": "73a1384ed6baa7655fc4",
+    "url": "/tuGerente_albumes/static/css/main.6dfd406a.chunk.css"
+  },
+  {
+    "revision": "911719862e36abdbccd1",
+    "url": "/tuGerente_albumes/static/js/2.ee7f1f00.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/tuGerente_albumes/static/js/2.ee7f1f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a1384ed6baa7655fc4",
+    "url": "/tuGerente_albumes/static/js/main.357287f3.chunk.js"
+  },
+  {
+    "revision": "884559cbc991f87c61f5",
+    "url": "/tuGerente_albumes/static/js/runtime-main.896bbe9c.js"
+  }
+]);
